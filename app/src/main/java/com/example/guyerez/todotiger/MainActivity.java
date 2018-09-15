1ae0880b7c19e726bb989744697cee00d16d0644
Guy <guy.oxnb@gmail.com>
2018-09-15 12:10:33 +0300

    Add FAB functionality - Add a new task list

--- app/src/main/java/com/example/guyerez/todotiger/MainActivity.java
+++ app/src/main/java/com/example/guyerez/todotiger/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.guyerez.todotiger;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //create a temporary list of task lists.
-        ArrayList<TaskList> taskLists = new ArrayList<TaskList>();
+        final ArrayList<TaskList> taskLists = new ArrayList<TaskList>();
         taskLists.add(new TaskList("Inbox"));
         taskLists.add(new TaskList("Homework"));
         taskLists.add(new TaskList("Legendary"));
@@ -32,6 +40,51 @@ public class MainActivity extends AppCompatActivity {
         // Make the {@link ListView} use the {@link TaskListAdapter} defined above, so that the
         // {@link ListView} will display list items for each {@link TaskList} in the list.
         listView.setAdapter(adapter);
+
+        //Set and create the FAB and it's action listener
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get add_list.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View addTaskListView = li.inflate(R.layout.add_list, null);
+
+                //Create the prompt to enable the user to create a new task list
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // Set add_list.xml as the layout for alertdialog builder
+                alertDialogBuilder.setView(addTaskListView);
+
+                //Set the user input box
+                final EditText userInput = (EditText) addTaskListView
+                        .findViewById(R.id.edit_list_name);
+
+                // Set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Create",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // Get list title from user and create a new task list
+                                        taskLists.add(new TaskList(userInput.getText().toString()));
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // Create the dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // Show the dialog
+                alertDialog.show();
+            }
+        });
 
     }
 }
